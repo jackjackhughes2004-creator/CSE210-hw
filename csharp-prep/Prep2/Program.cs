020b9c0e0b067b73452871e30c4738c67d3161d1
Jacob Hughes <jackjackhughes2004@gmail.com>
2026-09-21 10:42:28 -0600

    Grade Calculation Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         string letter;
+        string sign;
 
         Console.Write("Please enter your grade percentage: ");
         string userinput = Console.ReadLine();
@@ -22,8 +24,15 @@ class Program
             letter = "F";
         else
             letter = "";
+            
+        if (x%10 >= 7 && x < 90 && letter != "F")
+            sign = "+";
+        else if (x%10 <= 3 && letter != "F")
+            sign = "-";
+        else
+            sign = "";
         
-        Console.WriteLine($"Your grade is: {letter}");
+        Console.WriteLine($"Your grade is: {letter}{sign}");
 
         if (x>=70)
             Console.WriteLine("You Passed The Class!");
